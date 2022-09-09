0eea9d330b2e6752740f90affa36fa0a96ab57ac
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-09-09 06:05:14 -0700

    Printing alphabet letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+		putchar("%c", ch);
+		ch++;
+	return (0);
+}
