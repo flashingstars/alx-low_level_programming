032e0afae6baf37f84a677c92dc840b63fc118e5
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-09-14 15:10:27 -0700

    Print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,21 +10,5 @@
 
 int print_last_digit(int n)
 {
-	int a;
-
-	if (n < 0)
-	{
-		n = -n;
-	}
-
-	a = n % 10;
-
-	if (a < 10)
-	{
-		a = -a;
-	}
-
-	_putchar(a + '0');
-
-	return (a);
+	return (n % 10);
 }
