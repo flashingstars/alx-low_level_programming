c30e027906b8c44528f629e719d844b76743ec01
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-09-14 10:30:39 -0700

    Search for lowercase letter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - checks whether a character is lowercase
+ *
+ * @c: the character to check
+ *
+ * Return: 1 if c is lowercase, otherwise 0
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -4,8 +4,9 @@
  *
  * Returns: Always 0
  */
+
 int main(void)
 {
-	void print_alphabet_x10(void);
+	int _islower(int c);
 	return (0);
 }
