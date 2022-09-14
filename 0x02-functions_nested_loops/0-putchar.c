b6df2d128d6eda11a787b0a7086cc13d4b98de06
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-09-14 02:07:19 -0700

    Using putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print character to the screen
+ *
+ * Return: void
+ */
+void main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n'); /*trailing the '\n' needed in standard C */
+}
