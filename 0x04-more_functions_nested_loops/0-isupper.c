b15b8c231da312ff8493abdefc6e8ded5617ba72
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-09-15 13:04:29 -0700

    Finding uppercase letter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks whether a character is uppercase
+ *
+ * @c: the character to check
+ *
+ * Return: 1 if c is upper, otherwise 0
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+Creating nested loops without having access to standard output.
+
+Creating a header file and _putchar file.
