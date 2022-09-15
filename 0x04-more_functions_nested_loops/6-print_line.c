39e9decfa6180606285d53b7c291ec35596cf2f8
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-09-15 15:00:27 -0700

    Printing numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in terminal
+ *
+ * @n: number of times _ is printed
+ *
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	if (x > 0)
+	{
+		for(x = 0; x < n
+		_putchar('_' * n);
+	}
+	_putchar('\n');
+}
