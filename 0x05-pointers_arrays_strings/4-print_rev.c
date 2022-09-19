6dc904c8d0d2e01e9902f74852ed55cc8a062bad
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-09-19 10:59:22 -0700

    String reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints the reverse of a given string
+ *
+ * @s: string to be reversed
+ */
+
+void print_rev(char *s)
+{
+	int a, b, length;
+
+	a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	length = 1;
+
+	for (b = length - 1; b >= 0; b--)
+	{
+		_putchar(s[b]);
+	}
+
+	_putchar('\n');
+}
