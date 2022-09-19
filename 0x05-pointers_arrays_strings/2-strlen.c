3a0594081712560ce8ed991eec36985e8156f7f0
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-09-19 05:54:19 -0700

    String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * int_strlen - finds the length of a string
+ *
+ * @s: string pointer whose length is being sought
+ *
+ * Return: length of string
+ */
+
+int int_strlen(char *s)
+{
+	int a;
+
+	a = strlen(s);
+
+	return (a);
+}
