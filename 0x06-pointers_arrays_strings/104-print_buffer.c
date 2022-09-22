effaf5fcf50745d4f07980c2e526359cee8d2f6e
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-09-22 15:14:20 -0700

    Printing a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,28 +1,78 @@
 #include "main.h"
-/**
- * print_buffer - prints a buffer pointed by b
- *
- * @b: pointer
- * @size: bytes to be printed per line from buffer
- */
+#include <stdio.h>
 
+/**
+ * isPrintableASCII - determines if n is a printable ASCII char
+ * @n: integer
+ * Return: 1 if true, 0 if false
+ */
+int isPrintableASCII(int n)
+{
+	return (n >= 32 && n <= 126);
+}
+
+/**
+ * printHexes - print hex values for string b in formatted form
+ * @b: string to print
+ * @start: starting position
+ * @end: ending position
+ */
+void printHexes(char *b, int start, int end)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i < end)
+			printf("%02x", *(b + start + i));
+		else
+			printf("  ");
+		if (i % 2)
+			printf(" ");
+		i++;
+	}
+}
+
+/**
+ * printASCII - print ascii values for string b,
+ * formatted to replace nonprintable chars with '.'
+ * @b: string to print
+ * @start: starting position
+ * @end: ending position
+ */
+void printASCII(char *b, int start, int end)
+{
+	int ch, i = 0;
+
+	while (i < end)
+	{
+		ch = *(b + i + start);
+		if (!isPrintableASCII(ch))
+			ch = 46;
+		printf("%c", ch);
+		i++;
+	}
+}
+
+/**
+ * print_buffer - prints a buffer
+ * @b: string
+ * @size: size of buffer
+ */
 void print_buffer(char *b, int size)
 {
-	int i;
+	int start, end;
 
-	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	if (size > 0)
 	{
-		printf("%08x: ", i * 10);
-		if (i < size / 10)
+		for (start = 0; start < size; start += 10)
 		{
-			print_line(b, 9, i);
+			end = (size - start < 10) ? size - start : 10;
+			printf("%08x: ", start);
+			printHexes(b, start, end);
+			printASCII(b, start, end);
+			printf("\n");
 		}
-		else
-		{
-			print_line(b, size % 10 - 1, i);
-		}
-		putchar('\n');
-	}
-	if (size == 0)
-		putchar('\n');
+	} else
+		printf("\n");
 }
