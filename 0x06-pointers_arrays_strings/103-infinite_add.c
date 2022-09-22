33a4ad40778509c922720965fe9a09fef8b5d0fa
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-09-22 14:37:39 -0700

    Adding two numbers in a string

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * infinite_add - adds two numbers
+ *
+ * @n1: first string with number to be added
+ * @n2: second string with number to be added
+ * @r: buffer to store the result
+ * @size_r: current index of buffer
+ *
+ * Return: pointer to results if r can store the sum
+ * 0 - if r cannot store the sum
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int num, tens = 0;
+
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n1; n1--, r_index--)
+	{
+		num = (*n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n2; n2--, r_index--)
+	{
+		num = (*n2 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	if (tens && r_index >= 0)
+	{
+		*(r + r_index) = (tens % 10) + '0';
+		return (r + r_index);
+	}
+
+	else if (tens && r_index < 0)
+	{
+		return (0);
+	}
+
+	return (r + r_index + 1);
+}
