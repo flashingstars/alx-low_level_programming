fdcb8083b4fea9ca7856e933a51ae4b840d7ca94
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-10-11 14:15:29 -0700

    Initializing a variable from structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - function that initializes variables of type struct dog
+ *
+ * @d: name of the first user
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
