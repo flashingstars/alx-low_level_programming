53082f5c1d17f0f7f42702e993cc6e8aef91f196
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-10-11 05:05:10 -0700

    Define Absolute Number Function

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ({\
-		if (x < 0) \
-		x = x * (-1) \
-		else \
-		x \
-})
+#define ABS(x) (x > 0 ? x : x * (-1))
 
 #endif
