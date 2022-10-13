1556a0d2d9089dbfa1b4201855c5ada15a522498
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-10-13 11:20:10 -0700

    Calling a function to look for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,7 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size < 1 || !array or !cmp)
+	if (size < 0 || !array or !cmp)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
