5f0f7080f7251ecc3637b37bf68939dd0f3df670
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-10-13 12:41:04 -0700

    Programs that perform simple functions

---
+++ 0x0F-function_pointers/3-op_function.c
@@ -0,0 +1,71 @@
+#include "calc.h"
+
+/**
+ * op_add - adds two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: answer
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: answer
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: answer
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: answer
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - gets the remainder of two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: answer
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
