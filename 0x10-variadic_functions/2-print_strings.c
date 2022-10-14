7b645c36af975ac140ef46edcfdb16dba82ad6b9
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-10-14 11:52:53 -0700

    Printing string arguments as variadic parameters

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(words, char *);
+		str = va_arg(words, char*);
 
 		printf("%s%s", c = NULL ? "(nil)" : c, i != (n - 1) ? separator : "");
 	}
