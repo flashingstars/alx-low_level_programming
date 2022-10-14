72b3b291afdd4c97b4ce29883d18de8bdf734e49
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-10-14 12:00:58 -0700

    Printing string arguments as variadic parameters

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,34 +1,38 @@
-#include "variadic_functions.h"
-#include <stdio.h>
+#include "variadic_functions.c"
 
 /**
- * print_strings - prints a number of strings
- *
- * @separator: String printed between string
+ * print_strings - print all argument
+ * 
+ * @separator: string that separates the strings
  * @n: number of strings to print
  *
- * Return: nil if string is nil
+ * Return: nil if string is NULL
  */
+
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	char *str;
+	char *c;
 
-	va_list words;
 
-	va_start(words, n);
+	va_list strings;
+
+	va_start(strings, n);
 
 	if (!separator)
 		return;
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(words, char*);
-
-		printf("%s%s", c = NULL ? "(nil)" : c, i != (n - 1) ? separator : "");
+		c =  va_arg(strings,  char*);
+		printf("%s%s", c == NULL ? "(nil)" : c, i != (n - 1) ? separator : "");
 	}
 
+
+
+	va_end(strings);
 	printf("\n");
-	va_end(words);
+
+
 }
