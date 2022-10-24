209b0301d789f0e6994619c78f2d807694feb4fb
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-10-24 14:48:14 -0700

    Adding a node at the beginning of the list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,22 @@
 #include "lists.h"
 
+
+/**
+ * _strlen - finds the length of a string
+ *
+ * @str: string to find the length of
+ *
+ *  Return: length of string
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
 /**
  * add_node - Adds a node to the beginning of the list
  *
@@ -12,17 +29,21 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	struct list_s *temp = NULL;
-	int i;
+	list_t *new;
 
-	temp = (struct list_s *)malloc(sizeof(struct list_s));
-	if (temp == NULL)
+	if (str == NULL)
 		return (NULL);
-	temp->str = strdup(str);
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	temp->len = i;
-	temp->next = *head;
-	*head = temp;
-	return (temp);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = _strlen(new->str);
+	new->next = *head;
+	*head = new;
+	return (new);
 }
