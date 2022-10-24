06cc4319307c8b559d31ae692f8a490e1fb2ba4e
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-10-24 14:12:14 -0700

    Counting the nodes in the linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,16 +10,13 @@
 
 size_t print_list(const list_t *h);
 {
-	size_t count = 0;
-
-	while (h != NULL)
+	f (h)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
+		if (h->str)
 			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		count++;
+		else
+			printf("[0] (nil)\n");
+		return (print_list(h->next) + 1);
 	}
-	return (count);
+	return (0);
 }
