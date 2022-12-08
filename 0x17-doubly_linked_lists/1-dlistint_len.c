172d62a52a34f831599e32178f0b1c9355346e00
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-12-08 08:54:13 -0800

    Counting the elements in a linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - printing the number of elements present
+ *
+ * @h: pointer to first node
+ * Return: Number of elements in the list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (count);
+	while (h)
+	{
+		count++;
+		h->next;
+	}
+	return (count);
+}
