2dd63ccb267179d464e322229b3ecb12cce6e8a5
flashingstars <111060058+flashingstars@users.noreply.github.com>
2022-12-08 09:37:08 -0800

    Freeing a nodes in a list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees all nodes in a list
+ *
+ * @head: pointer to first node
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = heaad->next;
+		free(temp);
+	}
+}
